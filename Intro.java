89b76abc40a803191bf02bf879669f11fd52ad14
Wawerukelvin111 <146199308+Wawerukelvin111@users.noreply.github.com>
2024-02-23 17:06:14 +0300

    Add files via upload
    This file explains the basic startups of Java

---
+++ Intro.java
@@ -0,0 +1,40 @@
+public class Intro {
+
+    public static void main(String[] args){
+        System.out.println("I'm alive");
+        System.out.println(3+3);
+        //the print() method is simmilar to the println() but it does not insert a new line at the end of the output
+        System.out.print("Hello World! ");
+System.out.print("I will print on the same line.");
+//variable declaration
+String name="Kelvin";
+System.out.println(name);
+int num1=21;
+System.out.println(num1);
+//swap -123 with 57
+int num2=-123;
+num2=57;
+System.out.println(num2);
+//declaring a value final renders it unchangeable
+// final int myNum=15;
+// myNum=20;//will generate an errorcannot assign a value to a final variable
+//more
+// int myNum1 = 45;
+// float myFloatNum = 5.99f;
+// char myLetter = 'D';
+// boolean myBool = true;
+// String myText = "Hello";
+
+//To print both text and variable
+String name1="Suzzie";
+System.out.println("Hello "+name1);
+//combination by adding variables
+String firstName = "Mike ";
+String lastName = "Hacker";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+
+
+}
+}
+    
