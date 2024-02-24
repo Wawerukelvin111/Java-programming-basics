62d8230742621f0f3054f8f372d06d3ab32d4309
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-02-24 14:17:17 +0300

    data types
    get to learn and understand the various java datatypes

---
+++ datatypes.java
@@ -0,0 +1,58 @@
+public class datatypes {
+    public static void main(String[] args){
+        System.out.println("Learn about java data types");
+        // Primitive data types - includes byte, short, int, long, float, double, boolean and char
+        // Non-primitive data types - such as String, Arrays and Classes
+        /*byte - 1 byte- stores whole numbers from -128 to 127
+         * short - 2 bytes- stores hole numbers from -32,768 to 32,767
+         * int - 4 bytes- stores whole numbers from - -2,147,483,648 to 2,147,483,647
+         * long - 8 bytes- stores whole numbers from --9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+         * float - 4 bytes- stores ractional numbers .sufficient for storing 6-7 decimal digits
+         * double - 8 bytes- sufficient for storing 6-7 decimal digits
+         * boolean - 1 bit- stores true or false vaues 
+         * char - 2 bytes- stores a single character/letter or ASCII values 
+         * int myNum = 5;               // Integer (whole number)
+         * float myFloatNum = 5.99f;    // Floating point number
+         * char myLetter = 'D';         // Character
+         * boolean myBool = true;       // Boolean
+         * String myText = "Hello";     // String
+         */
+        //PRIMITIVE DATA TYPES
+        //numbers
+        byte mynum1=100;
+        System.out.println(mynum1);
+        short mynum2=5000;
+        System.out.println(mynum2);
+        int mynum3=100000;
+        System.out.println(mynum3);
+        long mynum4=1300000000;
+        System.out.println(mynum4);
+        //note the you should add d for double and f for float
+        float mynum5=7.89f;
+        System.out.println(mynum5);
+        double mynum6=17.4547d;
+        System.out.println(mynum6);
+        //for scientific numbers say "e"
+        float f1=35e3f;
+        double d1=12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+        //bools
+        boolean isJavaFun=true;
+        boolean JavaisHard=false;
+        System.out.println(isJavaFun);
+        System.out.println(JavaisHard);
+        //characters single quotes
+        char myGrade='A';
+        System.out.println(myGrade);
+        //Displaying characters using ASCII values
+        char myVar1=65, myVar2=66, myVar3=67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+        //strings double quotes
+        String greetings="Hi Friend";
+        System.out.println(greetings);
+
+    }
+}
