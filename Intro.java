1dd342272217bfef769d50ff34f2ad2f7cded94c
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-02-24 14:14:52 +0300

    few inclusions
    Additional functionality

--- Intro.java
+++ Intro.java
@@ -5,36 +5,54 @@ public class Intro {
         System.out.println(3+3);
         //the print() method is simmilar to the println() but it does not insert a new line at the end of the output
         System.out.print("Hello World! ");
-System.out.print("I will print on the same line.");
-//variable declaration
-String name="Kelvin";
-System.out.println(name);
-int num1=21;
-System.out.println(num1);
-//swap -123 with 57
-int num2=-123;
-num2=57;
-System.out.println(num2);
-//declaring a value final renders it unchangeable
-// final int myNum=15;
-// myNum=20;//will generate an errorcannot assign a value to a final variable
-//more
-// int myNum1 = 45;
-// float myFloatNum = 5.99f;
-// char myLetter = 'D';
-// boolean myBool = true;
-// String myText = "Hello";
+        System.out.print("I will print on the same line.\n");
+        //variable declaration
+        String name="Kelvin";
+        System.out.println(name);
+        int num1=21;
+        System.out.println(num1);
+        //swap -123 with 57
+        int num2=-123;
+        num2=57;
+        System.out.println(num2);
+        //declaring a value final renders it unchangeable
+        // final int myNum=15;
+        // myNum=20;//will generate an errorcannot assign a value to a final variable
+        //more
+        // int myNum1 = 45;
+        // float myFloatNum = 5.99f;
+        // char myLetter = 'D';
+        // boolean myBool = true;
+        // String myText = "Hello";
 
-//To print both text and variable
-String name1="Suzzie";
-System.out.println("Hello "+name1);
-//combination by adding variables
-String firstName = "Mike ";
-String lastName = "Hacker";
-String fullName = firstName + lastName;
-System.out.println(fullName);
+        //To print both text and variable
+        String name1="Suzzie";
+        System.out.println("Hello "+name1);
+        //combination by adding variables
+        String firstName = "Mike ";
+        String lastName = "Hacker";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        //adding integers
+        int x1 = 15;
+        int y2 = 16;
+        System.out.println(x1 + y2); // Print the value of x + y
+        //Declaring multiple variables
+        int x = 5;
+        int y = 6;
+        int z = 50;
+        System.out.println(x + y + z);
+        //simplified version of the above
+        int i = 25, j = 76, k = 450;
+        System.out.println(i + j + k);
+        //assigning one value to mutipe variables
+        int a, b, c;
+        a = b = c = 50;
+        System.out.println(a + b + c);
+        // All Java variables must be identified with unique names.
+        // These unique names are called identifiers.
+        // Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
 
-
-}
+    }
 }
     
