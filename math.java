525bc7a3cb7063d778e4506198c5ba3151676456
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-03-01 13:52:01 +0300

    few captures in math
    We added more knowledge about math

---
+++ math.java
@@ -0,0 +1,13 @@
+public class math {
+    public static void main(String[] args) {
+        System.out.println("The mathematic operations in java");
+        System.out.println(Math.max(5,10));//outputs the maximum value
+        System.out.println(Math.min(5,10));//outputs the minimum value
+        System.out.println(Math.sqrt(49));//outputs 7 as the result
+        System.out.println(Math.abs(-6.7));//The method returns absolutr (positive) value 
+
+        //Random number
+        System.out.println(Math.random());
+
+    }
+}
