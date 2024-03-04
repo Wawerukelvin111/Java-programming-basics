9109e278c531ba9e3850d736f283b261c95b00cc
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-03-04 07:45:43 +0300

    boolean
    This shows the detailed use of the boolean expressions

---
+++ booleans.java
@@ -0,0 +1,15 @@
+public class booleans {
+    public static void main(String[] args){
+        System.out.println("Boolean operations");
+        //Mostly of two values either yes/no,true/false,on/off...
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y); // returns true, because 10 is higher than 9
+        System.out.println(10 > 9); // returns true, because 10 is higher than 9
+
+        int x1 = 10;
+        System.out.println(x1 == 10); // returns true, because the value of x is equal to 10
+        System.out.println(x1 == 15); // returns false, because 10 is not equal to 15
+
+    }
+}
